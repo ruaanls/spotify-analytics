b211f3fefd039e036760c5f5af1a974ca2f2a3d9
ruaanls <ruanlima257@hotmail.com>
2026-03-27 21:01:02 -0300

    Estrutura Inicial do projeto

---
+++ src/main/java/br/com/spotifyanalytics/SpotifyanalyticsApplication.java
@@ -0,0 +1,13 @@
+package br.com.spotifyanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyanalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyanalyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/spotifyanalytics/SpotifyanalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.spotifyanalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyanalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
