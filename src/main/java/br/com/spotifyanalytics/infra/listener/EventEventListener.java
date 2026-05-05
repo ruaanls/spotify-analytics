2c4916d13f2bb889fdcbaf755374ad3249c8ebad
ruaanls <ruanlima257@hotmail.com>
2026-05-04 22:49:18 -0300

    Feat: Base para chamada ao Microsserviço de pagamentos com circuit breaker

--- src/main/java/br/com/spotifyanalytics/infra/listener/EventEventListener.java
+++ src/main/java/br/com/spotifyanalytics/infra/listener/EventEventListener.java
@@ -8,6 +8,7 @@ import br.com.spotifyanalytics.infra.persistence.entity.UsuariosJpa;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import io.awspring.cloud.sqs.annotation.SqsListener;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
